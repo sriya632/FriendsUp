58bcd2b776681ce47ef1ecfe1562fd13571bdce4
Sriya Choudary <sriyachoudary@Sriyas-MacBook-Air.local>
2025-03-10 18:40:12 -0400

    backend

--- Backend/app.py
+++ Backend/app.py
@@ -3,13 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app=Flask(__name__)
-CORS=(app)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db=SQLAlchemy(app)
 
 import routes
+
+with app.app_context():
+  db.create_all()
 
 if __name__ =="__main__":
     app.run(debug=True)
--- Backend/routes.py
+++ Backend/routes.py
@@ -5,7 +5,40 @@ from models import Friend
 
 @app.route("/api/friends/",methods=["GET"])
 def get_friends():
-    friends = Friend.query().all()
+    friends = Friend.query.all()
     result = [friend.to_json() for friend in friends]
-    return jsonify(result)
+    return jsonify(result),200
     
+# Create a friend
+@app.route("/api/friends",methods=["POST"])
+def create_friend():
+  try:
+    data = request.json
+    
+    required_fields = ["name","role","description","gender"]
+    for field in required_fields:
+      if field not in data or not data.get(field):
+        return jsonify({"error":f'Missing required field: {field}'}), 400
+    
+    name = data.get("name")
+    role = data.get("role")
+    description = data.get("description")
+    gender = data.get("gender")
+    
+    if gender == "male":
+      img_url = f"https://avatar.iran.liara.run/public/boy?username={name}"
+    elif gender == "female":
+      img_url = f"https://avatar.iran.liara.run/public/girl?username={name}"
+    else:
+      img_url = None
+
+    new_friend = Friend(name=name, role=role, description=description, gender= gender, img_url=img_url)
+
+    db.session.add(new_friend)
+    db.session.commit()
+    
+    return jsonify({"msg":"friend create success"}), 201
+    
+  except Exception as e:
+    db.session.rollback()
+    return jsonify({"error":str(e)}), 500
